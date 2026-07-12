45b765955f9f345f788bf4a7466f21de9b1d60ca
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:04 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "automation_variable_bools" {
-  description = "All automation_variable_bool resources"
-  value       = azurerm_automation_variable_bool.automation_variable_bools
-}
 output "automation_variable_bools_automation_account_name" {
-  description = "List of automation_account_name values across all automation_variable_bools"
-  value       = [for k, v in azurerm_automation_variable_bool.automation_variable_bools : v.automation_account_name]
+  description = "Map of automation_account_name values across all automation_variable_bools, keyed the same as var.automation_variable_bools"
+  value       = { for k, v in azurerm_automation_variable_bool.automation_variable_bools : k => v.automation_account_name }
 }
 output "automation_variable_bools_description" {
-  description = "List of description values across all automation_variable_bools"
-  value       = [for k, v in azurerm_automation_variable_bool.automation_variable_bools : v.description]
+  description = "Map of description values across all automation_variable_bools, keyed the same as var.automation_variable_bools"
+  value       = { for k, v in azurerm_automation_variable_bool.automation_variable_bools : k => v.description }
 }
 output "automation_variable_bools_encrypted" {
-  description = "List of encrypted values across all automation_variable_bools"
-  value       = [for k, v in azurerm_automation_variable_bool.automation_variable_bools : v.encrypted]
+  description = "Map of encrypted values across all automation_variable_bools, keyed the same as var.automation_variable_bools"
+  value       = { for k, v in azurerm_automation_variable_bool.automation_variable_bools : k => v.encrypted }
 }
 output "automation_variable_bools_name" {
-  description = "List of name values across all automation_variable_bools"
-  value       = [for k, v in azurerm_automation_variable_bool.automation_variable_bools : v.name]
+  description = "Map of name values across all automation_variable_bools, keyed the same as var.automation_variable_bools"
+  value       = { for k, v in azurerm_automation_variable_bool.automation_variable_bools : k => v.name }
 }
 output "automation_variable_bools_resource_group_name" {
-  description = "List of resource_group_name values across all automation_variable_bools"
-  value       = [for k, v in azurerm_automation_variable_bool.automation_variable_bools : v.resource_group_name]
+  description = "Map of resource_group_name values across all automation_variable_bools, keyed the same as var.automation_variable_bools"
+  value       = { for k, v in azurerm_automation_variable_bool.automation_variable_bools : k => v.resource_group_name }
 }
 output "automation_variable_bools_value" {
-  description = "List of value values across all automation_variable_bools"
-  value       = [for k, v in azurerm_automation_variable_bool.automation_variable_bools : v.value]
+  description = "Map of value values across all automation_variable_bools, keyed the same as var.automation_variable_bools"
+  value       = { for k, v in azurerm_automation_variable_bool.automation_variable_bools : k => v.value }
 }
 
